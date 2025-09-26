82254d7e0d2b796e12f95f92de63b3db58198bf0
Itamar Castillo 🐉 | Iridescent Cinderwyrm <157647426+Itamar96@users.noreply.github.com>
2025-09-26 04:43:22 -0400

    Update validate.js

--- web115/scripts/validate.js
+++ web115/scripts/validate.js
@@ -1,6 +1,6 @@
 function includeHTML() {
     var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
+    /* Loop for collection of HTML elements: */
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
         elmnt = z[i];
